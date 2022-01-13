0456f3733081f0e5caa66da9d78979f5f6cc32a8
Ali <aliammarali125@gmail.com>
2022-01-14 01:49:39 +0300

    add ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -8,10 +9,14 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('E menu Api')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   const configService: OurConfigService = app.get(OurConfigService);
+  app.useGlobalPipes(new ValidationPipe({
+    transform:true,
+  }));
   await app.listen(configService.getConfig().port);
 }
 bootstrap();
