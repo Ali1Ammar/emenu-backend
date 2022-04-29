e8fb9aa066f83d1b484cc89c4e315176ed351344
Ali <aliammarali125@gmail.com>
2022-04-29 23:40:07 +0300

    add secrent config for password

--- src/config.service.ts
+++ src/config.service.ts
@@ -5,19 +5,21 @@ import { ConfigService } from '@nestjs/config';
 export class OurConfigService {
   constructor(public service: ConfigService) {}
 
-   getConfig(): Config {
-    return this.service.get("all");
+  getConfig(): Config {
+    return this.service.get('all');
   }
 }
 
 export default () => ({
-  "all" : {
+  all: {
     port: parseInt(process.env.PORT) || 3000,
     jwtSecrent: process.env.jwtSecrent,
-  }
+    secretKey: process.env.SECRET_KEY,
+  },
 });
 
 interface Config {
   port: number;
   jwtSecrent: string;
+  secretKey: string;
 }
--- src/helper/hash_password.ts
+++ src/helper/hash_password.ts
@@ -2,10 +2,14 @@ import * as argon2 from 'argon2';
 
 export class PasswordHashHelper {
   static async hashPassword(password): Promise<string> {
-    return argon2.hash(password);
+    return argon2.hash(password, {
+      secret: Buffer.from(process.env.SECRET_KEY),
+    });
   }
 
   static async comparePassword(rawPassword, hashPassword) {
-    return argon2.verify(hashPassword, rawPassword);
+    return argon2.verify(hashPassword, rawPassword, {
+      secret: Buffer.from(process.env.SECRET_KEY),
+    });
   }
 }
